64cbfafc9bec86031b5ef91c89e95864f730c412
tristan <tristancalderbank@gmail.com>
2021-07-18 19:39:13 -0700

    fix skybox texture not being loaded as sRGB

--- skybox.h
+++ skybox.h
@@ -111,7 +111,7 @@ private:
 				return;
 			}
 
-			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_SRGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
 			stbi_image_free(data);
 		}
 
